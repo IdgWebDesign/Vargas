ae750b7d22d3dae113f5cd954c7a692a1f13937a
Web Design <idgwebdesign@gmail.com>
2023-08-18 12:17:39 -0300

    Commit temporal

--- js/main.js
+++ js/main.js
@@ -695,27 +695,7 @@
         document.getElementById("E4").src=URL4;
     }));
 
-    const swiper1 = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      });
+    
 
       const swiper2=  new Swiper('.swiperMENU', {
        // Optional parameters
@@ -738,14 +718,7 @@
          el: '.swiper-scrollbar2',
        },
       });
-    document.querySelector("#closer").addEventListener('click', () => {
-        document.querySelector(".ContEmergenteSlider").style.display = "none";
-        document.getElementById("E1").src=" ";
-        document.getElementById("E2").src=" ";
-        document.getElementById("E3").src=" ";
-        document.getElementById("E4").src=" ";
-
-    });         
+         
    
         
 })(jQuery);
