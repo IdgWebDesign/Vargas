e6571e1d9b610d5e618a6ae733a9986bce5303a0
Web Design <idgwebdesign@gmail.com>
2023-09-28 15:33:05 -0300

    nuevo commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -13,15 +13,15 @@ if($_POST) {
 
     // Check Name
     if (strlen($name) < 2) {
-        $error['name'] = "Please enter your name.";
+        $error['name'] = "Por favor, ingrese un nombre.";
     }
     // Check Email
     if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-        $error['email'] = "Please enter a valid email address.";
+        $error['email'] = "Por favor, ingrese un mail válido.";
     }
     // Check Message
     if (strlen($contact_message) < 15) {
-        $error['message'] = "Please enter your message. It should have at least 15 characters.";
+        $error['message'] = "Por favor, ingrese su mensaje. Debe tener al menos 15 caracteres";
     }
     // Subject
     if ($subject == '') { $subject = "Contact Form Submission"; }
